a8b9ee42022e48ea93d6b0d26a94f7e527fcf4c2
lMasterSparkl <592712515@qq.com>
2019-05-29 10:21:52 +0800

    spring cloud config 客户端通过服务端访问git配置文件 配置完成

---
+++ microservicecloud-config-client-3355/src/main/java/com/ygt/springcloud/ConfigClient_3355_StartSpringCloudApp2.java
@@ -0,0 +1,11 @@
+package com.ygt.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ConfigClient_3355_StartSpringCloudApp2 {
+public static void main(String[] args) throws Exception {
+	SpringApplication.run(ConfigClient_3355_StartSpringCloudApp2.class, args);
+}
+
+}
